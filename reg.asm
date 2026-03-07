59a2bb16bb0d742ad90b74db123c1ddd98ca3ea5
Vostrikova Kira <kirakaramisheva2007@gmail.com>
2026-03-07 12:20:02 +0300

    с гитом немного облажалась

--- reg.asm
+++ reg.asm
@@ -2,115 +2,97 @@
 .code
 org 100h
 VIDEOSEG equ 0b800h
-;дисклеймер, тут неподвижная, глупенькая рамка, выводятся 2 регистра и немного магических констант для вывода в определенную строку на экране, не пугайтесь, все поправлю
-;при таком расположении вызова 8 прерывания сразу выскакивает рамочка и один выводимый регистр увеличивается прямо на глазах, но по плану пользователь сначала должен нажать заветную клавишу с цифрой 2 для вывода рамки и только потом она должна обновлять регистры
+
 start:
 
-      ;mov ax, 3509h      
-       ;     int 21h       
-        ;    mov word ptr offset old09Ofs, bx
-         ;   mov bx, es
-          ;  mov word ptr offset old09Seg, bx
+      mov ax, 3509h      
+      int 21h       
+      mov word ptr offset old09Ofs, bx
+      mov word ptr offset old09Ofs1, bx
+      mov bx, es
+      mov word ptr offset old09Seg, bx
+      mov word ptr offset old09Seg1, bx
 
       push 0
       pop es
+
       cli
-      mov bx, 09h * 4
-      mov es: [bx], offset New09 ;прерывание клавиатуры
+      mov bx, 09h * 4               ;туда надо прерывание засунуть, используется только тут, я считаю, недостойно константы, очень удобно прям тут менять если что
+      mov es: [bx], offset New09 
       mov ax, cs
-      mov es: [bx + 2], ax
-      mov bx, 08h * 4
-      mov es: [bx], offset New08 ;прерывание таймера, чтоб в реалтайм табличка обновлялась, хз куда его впихнуть, чтобы оно работало только после вызова таблички (прерывание в прерывании не работает как так то) 
+      mov es: [bx + 2], ax          ;кидаем сюда сегмент, в котором находимся
+      mov bx, 08h * 4               ;оч круто
+      mov es: [bx], offset New08 
       mov ax, cs
       mov es: [bx + 2], ax
       sti
-      mov ax, 3100h
+
+      mov ax, 3100h                 ;сохранение кода
       mov dx, offset EndOfProgram
       shr dx, 4
       inc dx
       
 
-       ;mov ax, 4c00h
-       int 21h
+      int 21h
 
 
-New09   proc
+New09   proc                        ;обработчик прерываний (клавиатура)
         push ax 
         push bx 
         push es
         push 0b800h
         pop es
         mov bx, (5 * 80d + 40d) * 2
-        mov ah, 4ch
         in al, 60h 
-        mov es: [bx], ax
 
-        mov cl, 03h ;проверка на то, что считан скан-код клавиши с цифрой 2
+        mov cl, 03h                 ;проверка на то, что считан скан-код клавиши с цифрой 2
         cmp al, cl
-        jne gg
-        mov es: byte ptr [bx + 2], 03h
-        mov es: byte ptr [bx + 3], 5dh
-        call MainFunc2
+        jne Not2
+        call MainFunc               ;если 2 нажали, вызываем рамку
+        mov int08flag, 1            ;выставлен флаг для прерывания таймера
 
-        ;hlt удивительно, но если так сделать, все зависнет, невероятно
-        gg:
-        mov es: byte ptr [bx + 4], 02h
-        mov es: byte ptr [bx + 5], 0dh
+        Not2:
 
-
-        in al, 61h
+        in al, 61h                  ;мигаем старшим битом в 61 порту
         or al, 80h
         out 61h, al
         and al, not 80h
         out 61h, al
-        mov al, 20h
+        mov al, 20h                 ;сообщаем контроллеру прерываний, что обработка сигнала с клавиатуры закончена
         out 20h, al
+
         pop es 
         pop bx 
         pop ax
 
-        ;db 0eah
-        ;old09Ofs dw 0
-        ;old09Seg dw 0
-        iret
+
+        db 0eah                     
+        old09Ofs dw 0
+        old09Seg dw 0
+        ret
         endp
 
-New08   proc
+New08   proc                        ;обработчик прерываний (таймер)
         push ax 
         push bx 
         push es
         push 0b800h
         pop es
-        mov bx, (5 * 80d + 40d) * 2
-        mov ah, 4ch
-        mov al, 03h ;;;;и это тоже по приколу, на результат не влияет
-        mov es: [bx], ax
+        cmp int08flag, 1
+        jne NoFlag                  ;если флаг из 9 прерывания выставлен, то обновляем рамку
+        call MainFunc
+        NoFlag:
 
-        mov es: byte ptr [bx + 2], 03h ;это тут по рофлу, для проверки
-        mov es: byte ptr [bx + 3], 5dh
-        call MainFunc2
-        ;hlt удивительно, но если так сделать, все зависнет, невероятно
-        ;gg:
-        mov es: byte ptr [bx + 4], 02h
-        mov es: byte ptr [bx + 5], 0dh
-
-
-        in al, 71h
-        or al, 80h
-        out 71h, al
-        and al, not 80h
-        out 71h, al
-        mov al, 20h
-        out 20h, al
         pop es 
         pop bx 
         pop ax
 
-        ;db 0eah
-        ;old09Ofs dw 0
-        ;old09Seg dw 0
-        iret
+        db 0eah
+        old09Ofs1 dw 0
+        old09Seg1 dw 0
+        ret
         endp
+
 
 ;----------------------------------------------------------------------
 ;                         MainFunc
@@ -121,58 +103,77 @@ New08   proc
 ;----------------------------------------------------------------------
   MainFunc   proc 
 
-    mov ax, VIDEOSEG
-    mov es, ax
-
-    mov bx, (5 * 80d + 50d) * 2
-    mov dx, "ax"
-    call PrintReg        ;loop
-    mov bx, (6 * 80d + 50d) * 2
-    mov ax, si
-    inc si
-    mov dx, "si"
-    call PrintReg 
-    call PrintFrame
-
-      push 0 ;попытки сделать прерывание в прерывании (провалено), и дада, отступ сделан намеренно, тк этой фигни изначально тут не было
-      pop es
-      cli
-      mov bx, 09h * 4
-      mov es: [bx], offset New08
-      mov ax, cs
-      mov es: [bx + 2], ax
-      sti
-      mov ax, 3100h 
-      mov dx, offset EndOfProgram
-      shr dx, 4
-      inc dx
-      ;int 21h вылетает блин ну ваще
-      
-    ret
-    endp
-
-
-;----------------------------------------------------------------------
-;                         MainFunc2
-;to think
-;Enter:
-;Exit:  
-;Destr: 
-;----------------------------------------------------------------------
-  MainFunc2   proc 
+    push ax ;чтобы сохранить начальные значения регистров
+    push bx
+    push dx
 
     mov ax, VIDEOSEG
     mov es, ax
 
-    mov bx, (5 * 80d + 50d) * 2
+    ; да, копипаст, невкусно, но мб оправдано:
+    ; 1. мне не приходится пушить cx в стек, тк нет цикла, да это ж круто!!
+    ; 2. я не знаю, как сделать подстановку регистров, типа чтобы в ax каждый раз в цикле разные регистры были
+    ; 3. это стильно, модно и молодежно
+
+    mov bx, STRING5
     mov dx, "ax"
-    call PrintReg        ;loop
-    mov bx, (6 * 80d + 50d) * 2
+    call PrintReg   
+
+    mov bx, STRING5 + LEN_OF_STR
+    pop dx
+    pop ax
+    push ax
+    push dx
+    mov dx, "bx"
+    call PrintReg
+
+    mov bx, STRING5 + LEN_OF_STR * 2
+    mov ax, cx
+    mov dx, "cx"
+    call PrintReg
+
+    mov bx, STRING5 + LEN_OF_STR * 3
+    pop ax
+    mov dx, "dx"
+    call PrintReg
+    push ax
+
+    mov bx, STRING5 + LEN_OF_STR * 4
     mov ax, si
     inc si
     mov dx, "si"
     call PrintReg 
+
+    mov bx, STRING5 + LEN_OF_STR * 5
+    mov ax, di
+    mov dx, "di"
+    call PrintReg
+
+    mov bx, STRING5 + LEN_OF_STR * 6
+    mov ax, bp
+    mov dx, "bp"
+    call PrintReg
+
+    mov bx, STRING5 + LEN_OF_STR * 7
+    mov ax, sp
+    mov dx, "sp"
+    call PrintReg
+
+    mov bx, STRING5 + LEN_OF_STR * 8
+    mov ax, ds
+    mov dx, "ds"
+    call PrintReg
+
+    mov bx, STRING5 + LEN_OF_STR * 9
+    mov ax, es
+    mov dx, "es"
+    call PrintReg
+
     call PrintFrame
+
+    pop dx
+    pop bx
+    pop ax
       
     ret
     endp
@@ -182,12 +183,12 @@ New08   proc
 ;print reg
 ;Enter: ax - this reg
 ;       dh, dl - name of reg (first letter in ah)
+;       bx - start pos
 ;Exit:  
 ;Destr: 
 ;----------------------------------------------------------------------
   PrintReg   proc 
 
-    ;mov bx, (7 * 80d + 30d) * 2
     mov es: byte ptr [bx], dh
     mov es: byte ptr [bx + 1], 0dh
     add bx, 2
@@ -195,20 +196,20 @@ New08   proc
     mov es: byte ptr [bx + 1], 0dh
     add bx, 10
 
-    mov saved_reg, ax
+    mov SAVED_REG, ax
 
     shl al, 4               ;first on the right
     shr al, 4
     call PrintHalfOfReg
 
     sub bx, 2
-    mov ax, saved_reg
+    mov ax, SAVED_REG
             
     shr al, 4               ;second on the right
     call PrintHalfOfReg
 
     sub bx, 2
-    mov ax, saved_reg
+    mov ax, SAVED_REG
 
     shl ah, 4               ;second on the left
     shr ah, 4
@@ -216,13 +217,13 @@ New08   proc
     call PrintHalfOfReg
 
     sub bx, 2
-    mov ax, saved_reg
+    mov ax, SAVED_REG
 
     shr ah, 4               ;first on the left
     mov al, ah
     call PrintHalfOfReg
 
-    mov ax, saved_reg
+    mov ax, SAVED_REG
       
     ret
     endp
@@ -263,14 +264,14 @@ New08   proc
 ;----------------------------------------------------------------------
   PrintLineX	proc
 
-    mov cx, xlen
+    mov cx, XLEN
   	Printline:
-	mov es: byte ptr [bx], xline
-	mov es: byte ptr [bx + 1], color
-	add bx, 2
-	loop PrintLine
-	ret
-	endp
+		mov es: byte ptr [bx], XLINE
+		mov es: byte ptr [bx + 1], COLOR
+		add bx, 2
+		loop PrintLine
+		ret
+		endp
 
 
 ;----------------------------------------------------------------------
@@ -281,28 +282,14 @@ New08   proc
 ;Destr: cx becomes 0, bx becomes bx + 2 * cx(start)
 ;----------------------------------------------------------------------
   PrintLineY	proc
-    mov cx, ylen
+    mov cx, YLEN
   	Printlinee:
-	mov es: byte ptr [bx], yline
-	mov es: byte ptr [bx + 1], color
-	add bx, 80*2
-	loop PrintLinee
-	ret
-	endp
-
-;----------------------------------------------------------------------
-;                              PrintAngle
-;	print angle
-;Entry: bx - position
-;       al - symbol
-;Exit: ???
-;Destr: 
-;----------------------------------------------------------------------
-  PrintAngle	proc
-	mov es: byte ptr [bx], al
-	mov es: byte ptr [bx + 1], color
-	ret
-	endp
+		mov es: byte ptr [bx], YLINE
+		mov es: byte ptr [bx + 1], COLOR
+		add bx, 80*2
+		loop PrintLinee
+		ret
+		endp
 
 
 ;----------------------------------------------------------------------
@@ -314,50 +301,39 @@ New08   proc
 ;----------------------------------------------------------------------
   PrintFrame	proc
       
-    mov bx, lup
+    mov bx, LUP
     call PrintLineX
-    mov bx, ldown
+    mov bx, LDOWN
     call PrintLineX
-    mov bx, lup
+    mov bx, LUP
     call PrintLineY
-    mov bx, lup + xlen * 2
+    mov bx, LUP + XLEN * 2
     call PrintLineY
 
-    lea di, pos_symb_angles
-    mov cx, 4
-    PrintAngles:
-    mov bx, word ptr [di]
-    add di, 2
-    mov ax, word ptr [di]
-    add di, 2
-    call PrintAngle
-    loop PrintAngles
-	ret
-	endp
+		ret
+		endp
 
 .data
-saved_reg dw 0
-lup = (3 * 80d + 42d) * 2
-ldown = (12 * 80d + 42d) * 2
-ylen = 9
-xlen = 16
-rup = lup + xlen * 2 
-rdown = ldown + xlen * 2 
-color = 0dh
-xline = 0c4h
-yline = 0b3h
+SAVED_REG dw 0
+LUP = (3 * 80d + 42d) * 2
+LDOWN = (21 * 80d + 42d) * 2
+YLEN = 18
+XLEN = 16
+RUP = LUP + XLEN * 2 
+RDOWN = LDOWN + XLEN * 2 
+COLOR = 0dh
+XLINE = 0c4h
+YLINE = 0b3h
 LUPAngle = 0dah
 RUPAngle = 0bfh
 LDOWNAngle = 0c0h
 RDOWNAngle = 0d9h
-pos_symb_angles dw lup, LUPAngle, ldown, LDOWNAngle, rup, RUPAngle, rdown, RDOWNAngle
-lett1 db 03h
-lett2 db 10h 
-flag1 db 0
-flag2 db 0
 int08flag db 0
+STRING5 = (5 * 80d + 50d) * 2
+LEN_OF_STR = 160
+;regs db di, bp, sp, ds, es такое досбокс не одобряет, это печально, придется оставлять копипастт
+;name_of_reg dw "di", "bp", "sp", "ds", "es"
 
 EndOfProgram:
 end    start
-
 
